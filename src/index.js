7709556b7d62b43f3e17e35fae35afdb2561f721
amir shahsahebi <shahsahebiamir@gmail.com>
2022-04-17 02:22:12 +0430

    first session done

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ function getButtonText() {
 const App = () => {
     const buttonText = {text: "click me"}
     const style = { backgroundColor: 'red', color: 'white' }
+    const labelText = "Enter Name";
   return (
     <div>
-      <label className="label" for="name">
-        Enter Name
+      <label className="label" htmlFor="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
       {/* <button style="background-color: red; color:white">Submit</button> */}
