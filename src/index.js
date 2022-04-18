0ccedc085680154c91e5c2caf10b7387b46d7429
amir shahsahebi <shahsahebiamir@gmail.com>
2022-04-18 18:39:53 +0430

    comment componetnt added

--- src/index.js
+++ src/index.js
@@ -19,6 +19,48 @@ const App = () => {
           <div className="text">Good post</div>
         </div>
       </din>
+      <din className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 8:00AM</span>
+          </div>
+          <div className="text">Good post</div>
+        </div>
+      </din>
+      <din className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 8:00AM</span>
+          </div>
+          <div className="text">Good post</div>
+        </div>
+      </din>
+      <din className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 8:00AM</span>
+          </div>
+          <div className="text">Good post</div>
+        </div>
+      </din>
     </div>
   );
 };
