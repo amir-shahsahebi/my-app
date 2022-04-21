39f9c88700fae8fd3127e03518ac3fc8ced4f988
amir shahsahebi <shahsahebiamir@gmail.com>
2022-04-21 13:06:40 +0430

    seasons determined

--- src/index.js
+++ src/index.js
@@ -3,35 +3,26 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super();
-    // super(props);
-    // this is the only time we do direct assignment to this.state
-    this.state = { lat: null , errorMessage: "" };
-
+  state = { lat: null, errorMessage: "" };
+  // instead of constructor function, we use "componentDidMount ()" 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setstate!!!
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message })
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
-
   // React says we have to define render!!
   render() {
-      if (this.state.errorMessage && !this.state.lat) {
-        return <div>Error: {this.state.errorMessage}</div>;
-      } 
-
-      if (!this.state.errorMessage && this.state.lat) {
-        return <div>Latitude: {this.state.lat}</div>;
-      }
-
-      return <div>Loading!</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
     }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+
+    return <div>Loading!</div>;
   }
+}
     
     ReactDOM.render(<App />, document.querySelector("#root"));
