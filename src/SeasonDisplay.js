b92b03c485697b2a046e28e33415606c8bef480a
amir shahsahebi <shahsahebiamir@gmail.com>
2022-04-21 23:26:34 +0430

    season config set to an object

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,19 +1,34 @@
 import React from "react";
 
-const getSeason = (lat, month) => {
-    if (month > 2 && month < 9) {
-      return lat > 0 ? "summer" : "winter";
-    } else {
-      return lat > 0 ? "winter" : "summer";
-    }
+const seasonConfig = {
+    summer: {
+        text: "its so hot!!!",
+        iconName: "sun",
+    },
+    winter: {
+        text: "oh, its so cold",
+        iconName: "snowflake",
+    },
 }
 
-const SeasonDisplay = props => {
-    const season = getSeason(props.lat, new Date().getMonth())
-    console.log(season);
-    return (
-        <div>Season Display</div>
-    )
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const {text, iconName} = seasonConfig[season]
+  return (
+    <div>
+      <i className={`massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`massive ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
