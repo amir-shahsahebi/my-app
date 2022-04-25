15095037fad81c1d85ee0374873c116c0191b27c
amir shahsahebi <shahsahebiamir@gmail.com>
2022-04-25 23:53:55 +0430

    async function added for fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import Searchbar from "./Searchbar"
+import axios from "axios";
+import Searchbar from "./Searchbar";
+
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
+    async onSearchSubmit(term) {
+        // console.log(term);
+        const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+          params: {
+            query: term, // and also we can use query in the link above with: `/photoes?query=term`
+          },  
+          headers: {
+            Authorization:
+              "Client-ID 8-8_L71rwFXFBZx71ejqr5gyxnoUp6bi8kqRK__l4UM",
+          },
+        })
+        console.log(response.data.results);
+        // .then(response=>console.log(response.data.results)).catch(e => console.log(e)) // notice: we use .then() if we don't use "async" and "await"
     }
     render () {
         return (
