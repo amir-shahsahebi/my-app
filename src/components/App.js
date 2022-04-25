fb48fa3502656cae5609a01f9f61fcaf73c467d0
amir shahsahebi <shahsahebiamir@gmail.com>
2022-04-26 00:35:42 +0430

    this fixed with setting an arrow func

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,30 @@ import axios from "axios";
 import Searchbar from "./Searchbar";
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
-        // console.log(term);
-        const response = await axios.get(`https://api.unsplash.com/search/photos`, {
-          params: {
-            query: term, // and also we can use query in the link above with: `/photoes?query=term`
-          },  
-          headers: {
-            Authorization:
-              "Client-ID 8-8_L71rwFXFBZx71ejqr5gyxnoUp6bi8kqRK__l4UM",
-          },
-        })
-        console.log(response.data.results);
-        // .then(response=>console.log(response.data.results)).catch(e => console.log(e)) // notice: we use .then() if we don't use "async" and "await"
-    }
-    render () {
-        return (
-          <div style={{ marginTop: "10px" }}>
-            <Searchbar onSubmit={this.onSearchSubmit} />
-          </div>
-        );
-    }
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {  // we use again an arrow function because "this" refer in App component whe we call "onSubmit" later  
+    // console.log(term);
+    const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+      params: {
+        query: term, // and also we can use query in the link above with: `/photoes?query=term`
+      },
+      headers: {
+        Authorization: "Client-ID 8-8_L71rwFXFBZx71ejqr5gyxnoUp6bi8kqRK__l4UM",
+      },
+    });
+    this.setState({ images: response.data.results });
+    // console.log(response.data.results);
+    // .then(response=>console.log(response.data.results)).catch(e => console.log(e)) // notice: we use .then() if we don't use "async" and "await"
+  };
+  render() {
+    return (
+      <div style={{ marginTop: "10px" }}>
+        <Searchbar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} Images
+      </div>
+    );
+  }
 }
 
 export default App;
