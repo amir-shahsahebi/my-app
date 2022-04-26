7a85b50dee98db9a66d4f026bf603dd41827a167
amir shahsahebi <shahsahebiamir@gmail.com>
2022-04-26 18:55:01 +0430

    use ref to find elements in dom and addEv for load

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener("load", this.setSpans)
+        // console.log(this.imageRef);
+        // console.log(this.imageRef.current.clientHeight); // it will be 0, because image haven't been reach yet 
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render() {
-        retutn (
+        const {description, urls} = this.props.image;
+        return (
             <div>
                 <img 
-                src={this.props.image.urls.regular} 
-                alt={this.props.image.description} />
+                ref={this.imageRef}
+                src={urls.regular} 
+                alt={description} />
             </div>
         )
     } 
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -5,7 +5,7 @@ import ImageCard from './ImageCard';
 const ImageList = (props) => {
     const images = props.images.map((image) => {
       return (
-        <ImageCard key={image.id} image={image} />
+        <ImageCard key={image.id} image={image} /> // id must be in parent that repeat
       );
     });   
     return <div className='image-list'>{images}</div>
