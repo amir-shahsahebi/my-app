cf8b54ca1d79d06a0353a89b89b9c3cd531bd3ef
amir shahsahebi <shahsahebiamir@gmail.com>
2022-05-01 17:42:48 +0430

    dropdown compoent added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -17,11 +18,24 @@ const items = [
   },
 ];
 
+const options = [
+  { label: "the color red", value: "red" },
+  {
+    label: "the color green",
+    value: "green",
+  },
+  {
+    label: "a shade of blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown options={options} />
     </div>
   );
 };
