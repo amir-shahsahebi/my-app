25cac9d9cdc02ce096175aa5ccaf6e138841d997
amir shahsahebi <shahsahebiamir@gmail.com>
2022-05-05 10:24:13 +0430

    navigation completed without routerdom

---
+++ src/components/Link.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Link = ({ className, href, children }) => {
+  const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
+    event.preventDefault();
+    window.history.pushState({}, "", href); // this is a built in function imn window that just move to href link without refreshing all page and ger all requests
+
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
+  };
+  return (
+    <a onClick={onClick} className={className} href={href}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
